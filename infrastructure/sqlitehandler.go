118e5932103656a750e6ef6203981d9e0ccaa4d8
Andrea Zucchini <azga84@gmail.com>
2017-10-25 15:45:17 +0200

    access point to sqlite

---
+++ infrastructure/sqlitehandler.go
@@ -0,0 +1,45 @@
+package infrastructure
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type SqliteHandler struct {
+	Conn *sql.DB
+}
+
+func (handler *SqliteHandler) Execute(statement string) {
+	handler.Conn.Exec(statement)
+}
+
+func (handler *SqliteHandler) Query(statement string) Row {
+	rows, err := handler.Conn.Query(statement)
+	if err != nil {
+		fmt.Println(err)
+		return new(SqliteRow)
+	}
+	row := new(SqliteRow)
+	row.Rows = rows
+	return row
+}
+
+type SqliteRow struct {
+	Rows *sql.Rows
+}
+
+func (r SqliteRow) Scan(dest ...interface{}) {
+	r.Rows.Scan(dest...)
+}
+
+func (r SqliteRow) Next() bool {
+	return r.Rows.Next()
+}
+
+func NewSqliteHandler(dbfileName string) *SqliteHandler {
+	conn, _ := sql.Open("sqlite3", dbfileName)
+	sqliteHandler := new(SqliteHandler)
+	sqliteHandler.Conn = conn
+	return sqliteHandler
+}
