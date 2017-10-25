ca3433cbe8a80f45b9e73c39203c57b67cc88b44
Andrea Zucchini <azga84@gmail.com>
2017-10-25 12:07:30 +0200

    necessary types

--- usescases/orderInteractor.go
+++ usescases/orderInteractor.go
@@ -1,1 +1,31 @@
 package usescases
+
+import "github.com/zucchinidev/clean-architecture-golang/domain"
+
+type UserRepository interface {
+	Store(user User)
+	FindById(id int) User
+}
+
+type User struct {
+	Id       int
+	IsAdmin  bool
+	Customer domain.Customer
+}
+
+type Item struct {
+	Id    int
+	Name  string
+	Value float64
+}
+
+type Logger interface {
+	Log(message string) error
+}
+
+type OrderInteractor struct {
+	UserRepository  UserRepository
+	OrderRepository domain.OrderRepository
+	ItemRepository  domain.ItemRepository
+	Logger          Logger
+}
